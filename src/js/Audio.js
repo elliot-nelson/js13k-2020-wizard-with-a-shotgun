d2018927483c2d595de1e63e194da922e33b735f
Elliot Nelson <nelson.work@gmail.com>
2020-09-07 08:51:56 -0400

    :notes: simplify song, but still +408 bytes over

--- src/js/Audio.js
+++ src/js/Audio.js
@@ -2,7 +2,7 @@
 
 import { zzfx, zzfxP, zzfxX } from './lib/zzfx';
 import { zzfxM } from './lib/zzfxm';
-import { ObliqueButNotObtuse } from './songs/ObliqueButNotObtuse';
+import { ObliqueMystique } from './songs/ObliqueMystique';
 
 export const Audio = {
     async init() {
@@ -16,7 +16,7 @@ export const Audio = {
         Audio.shotgun = [,0.01,140,0.01,0.02,0.45,4,2.42,0.1,-0.1,,,,1.2,,0.3,0.04,0.8,0.02];
         Audio.page = [,,1233,,.01,.2,1,1.43,,,539,.1,,,,,,.51,.03,.01];
         Audio.shellReload = [,,68,0.01,,0.14,1,1.53,7.5,0.1,50,0.02,-0.01,-0.2,0.1,0.2,,0.47,0.01];
-        Audio.song = zzfxM(...ObliqueButNotObtuse);
+        Audio.song = zzfxM(...ObliqueMystique);
     },
 
     update() {
