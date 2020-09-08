f8046c7a6c35b85c4b51c026b780cd77035c4f50
Elliot Nelson <nelson.work@gmail.com>
2020-09-08 14:43:11 -0400

    :bug: player cannot pick up pages when dashing

--- src/js/Page.js
+++ src/js/Page.js
@@ -27,7 +27,7 @@ export class Page {
         this.vel.y *= 0.95;
 
         let v = vectorBetween(this.pos, game.player.pos);
-        if (v.m < game.player.radius + this.radius + 2 && game.player.state === Behavior.HUNT) {
+        if (v.m < game.player.radius + this.radius + 2 && game.player.state !== Behavior.DEAD && game.player.state !== Behavior.SPAWN) {
             this.cull = true;
             game.entities.push(new PageCollectedAnimation(this.pos, this.amount));
         }
